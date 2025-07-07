26a946c2433814ea50d645b8ca48faa784d6a8b7
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2025-07-07 22:49:13 +0530

    Update analyze_poetry_files.py

--- projects/analyze_poetry_files/analyze_poetry_files.py
+++ projects/analyze_poetry_files/analyze_poetry_files.py
@@ -3,27 +3,24 @@
 # Open and read file
 with open("poem.txt", "r") as file:
     content = file.read()
-    print(content)
 
 # Split text into words
-w = content.split()
-words = [word.lower() for word in w]
+words = [word.lower() for word in content.split()] 
 
 # Use dictionaries to count words
-def count_words():
-    data_dict = {}
-    for word in words:
-        if word in data_dict:
-            data_dict[word] += 1
-        else:
-            data_dict[word] = 1
+def count_words():  
+    data_dict = {}  
+    for word in words:  
+        data_dict[word] = data_dict.get(word, 0) + 1 
     return data_dict
 
 data = count_words()
 
 # Goal1. Counts how many times the word "you" appears:
-data["you"]
+you_count = data.get("you", 0)
 
 # Goal2. Finds the most frequent word.
-from collections import Counter
-Counter(words).most_common(1)
+from collections import Counter  
+stopwords = {"the", "and", "a", "to", "i"}  # Add more as needed  
+filtered_words = [word for word in words if word not in stopwords]  
+most_common = Counter(filtered_words).most_common(1)

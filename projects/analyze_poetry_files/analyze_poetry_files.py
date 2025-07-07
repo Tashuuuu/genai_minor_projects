bb0cc72ad7d251d2c055f83ee6aabba2ebd1eb8d
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2025-07-07 15:47:49 +0530

    Update analyze_poetry_files.py

--- projects/analyze_poetry_files/analyze_poetry_files.py
+++ projects/analyze_poetry_files/analyze_poetry_files.py
@@ -2,3 +2,22 @@
 # 1. Counts how many times the word "you" appears.
 # 2. Finds the most frequent word (excluding common words like "the", "and").
 
+# Open and read file
+with open("poem.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+# Split text into words
+words = content.split()
+
+# Use dictionaries to count words
+def count_words():
+    data_dict = {}
+    for word in words:
+        if word in data_dict:
+            data_dict[word] += 1
+        else:
+            data_dict[word] = 1
+    return data_dict
+
+count_words()

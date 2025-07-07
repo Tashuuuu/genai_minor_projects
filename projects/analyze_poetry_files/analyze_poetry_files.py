18c7605e63762273efad8b04015f7530cd7107a2
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2025-07-07 13:59:29 +0530

    Update and rename Analyze Poetry File to projects/analyze_poetry_files/analyze_poetry_files.py

---
+++ projects/analyze_poetry_files/analyze_poetry_files.py
@@ -0,0 +1,4 @@
+# Goal: Write a Python script that reads .txt file and:
+# 1. Counts how many times the word "you" appears.
+# 2. Finds the most frequent word (excluding common words like "the", "and").
+
